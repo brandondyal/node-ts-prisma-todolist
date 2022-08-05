1f7cf37e78bb682be5e8d093ed7ad795490156fc
Brandon Dyal <brandondyal@gmail.com>
2022-08-04 22:44:45 -0400

    Replace ts-jest with @swc/jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,19 @@
-const config = {
-  preset: 'ts-jest',
+// swc issues
+// https://github.com/swc-project/swc/issues/3559
+// https://github.com/swc-project/jest/issues/62
+// https://github.com/swc-project/cli/issues/20
+
+const jestConfig = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest', { configFile: '.swcrc-jest' }],
   },
   setupFilesAfterEnv: ['./setupTests.ts'],
   testMatch: ['/**/*test.(js|ts)'],
   verbose: true,
 };
 
-export default config;
+export default jestConfig;
