e7b50ccf81fde351c246a4619ca9971b6f962cb9
Brandon Dyal <brandondyal@gmail.com>
2022-08-25 19:16:44 -0400

    Add swagger-ui-express

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 
 import { errorHandler, invalidPathHandler } from '@/middleware/error';
 import todoController from '@/todos/todoController';
 import todoListController from '@/todoLists/todoListController';
 import userController from '@/users/userController';
+import apiSpec from '@/swagger/spec.json';
 
 const app = express();
 
@@ -21,6 +23,11 @@ app.use(express.json());
 app.use('/todos', todoController);
 app.use('/todoLists', todoListController);
 app.use('/users', userController);
+
+if (process.env.NODE_ENV === 'dev') {
+  app.use('/', swaggerUi.serve, swaggerUi.setup(apiSpec));
+}
+
 app.use(errorHandler, invalidPathHandler);
 
 export { app };
